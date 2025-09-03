8393b8d077f4fe5d7f4239e39da3dba450a4631e
365Agents <rgarcia@365agents.com>
2025-09-03 05:49:07 -0700

    Final fix - Remove invalid options and properly configure

--- next.config.js
+++ next.config.js
@@ -1,39 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Disable SWC minification
   swcMinify: false,
-  experimental: {
-    // Force disable SWC completely
-    swcFileReading: false,
-    swcMinify: false,
-    swcLoader: false,
-  },
+  
+  // Compiler configuration
   compiler: {
     removeConsole: false,
   },
+  
+  // Environment variables to disable SWC
+  env: {
+    NEXT_DISABLE_SWC: 'true',
+  },
+  
+  // Webpack configuration to force Babel
   webpack: (config, { dev, isServer }) => {
-    // Force Babel loader for all JS/TS files
+    // Add Babel loader for JS/TS files
     config.module.rules.push({
       test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
         options: {
           presets: ['next/babel'],
         },
       },
-    })
+    });
     
-    // Completely disable SWC
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@next/swc-linux-x64-gnu': false,
-      '@next/swc-linux-x64-musl': false,
-    }
-    
-    return config
-  },
-  env: {
-    // Force disable SWC at environment level
-    NEXT_DISABLE_SWC: 'true',
+    return config;
   },
 }
 
