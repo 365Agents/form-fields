61c23a300e33d38884fea5ebd70c5c157fb40391
365Agents <rgarcia@365agents.com>
2025-09-03 05:44:53 -0700

    Properly disable SWC completely

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false,
+  compiler: {
+    // Disable SWC compiler completely
+    removeConsole: false,
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Force Babel compilation
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    }
+    return config
   },
 }
 
