c63a41843c7250fccdd46b4f23ea15c66bf84c3d
365Agents <rgarcia@365agents.com>
2025-09-03 05:53:15 -0700

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,33 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable SWC minification
-  swcMinify: false,
-  
-  // Compiler configuration
-  compiler: {
-    removeConsole: false,
-  },
-  
-  // Environment variables to disable SWC
-  env: {
-    NEXT_DISABLE_SWC: 'true',
-  },
-  
-  // Webpack configuration to force Babel
-  webpack: (config, { dev, isServer }) => {
-    // Add Babel loader for JS/TS files
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-        },
-      },
-    });
-    
-    return config;
+  reactStrictMode: true,
+  experimental: {
+  reactStrictMode: true,
+  swcMinify: false, // Disable SWC minifier
   },
 }
 
