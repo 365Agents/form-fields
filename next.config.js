e5ce7d09a03b270134b879b8b2d04d08963bc771
365Agents <rgarcia@365agents.com>
2025-09-03 05:56:27 -0700

    Fix next.config.js syntax and configuration errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-  reactStrictMode: true,
-  swcMinify: false, // Disable SWC minifier
+  swcMinify: false,
+  compiler: {
+    removeConsole: false,
   },
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config, { isServer }) => {
+    // Completely disable SWC loader
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.use && rule.use.loader && rule.use.loader.includes('next-swc-loader')) {
+        rule.use.loader = 'babel-loader'
+        rule.use.options = {
+          presets: ['next/babel']
+        }
+      }
+      return rule
+    })
+    
+    return config
+  }
 }
 
 module.exports = nextConfig
