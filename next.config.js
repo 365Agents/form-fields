9eead3a4d339a98768fc26240afc01b43fc30386
365Agents <rgarcia@365agents.com>
2025-09-03 05:59:24 -0700

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -6,21 +6,7 @@ const nextConfig = {
     removeConsole: false,
   },
   experimental: {
-    esmExternals: 'loose',
-  },
-  webpack: (config, { isServer }) => {
-    // Completely disable SWC loader
-    config.module.rules = config.module.rules.map(rule => {
-      if (rule.use && rule.use.loader && rule.use.loader.includes('next-swc-loader')) {
-        rule.use.loader = 'babel-loader'
-        rule.use.options = {
-          presets: ['next/babel']
-        }
-      }
-      return rule
-    })
-    
-    return config
+    esmExternals: false,
   }
 }
 
