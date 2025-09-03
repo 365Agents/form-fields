dd6f0620b93b829f27cb1ff150c26993fe5f695e
365Agents <rgarcia@365agents.com>
2025-09-03 05:46:20 -0700

    Force disable SWC completely with environment variables

--- next.config.js
+++ next.config.js
@@ -1,16 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false,
+  experimental: {
+    // Force disable SWC completely
+    swcFileReading: false,
+    swcMinify: false,
+    swcLoader: false,
+  },
   compiler: {
-    // Disable SWC compiler completely
     removeConsole: false,
   },
   webpack: (config, { dev, isServer }) => {
-    // Force Babel compilation
+    // Force Babel loader for all JS/TS files
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+        },
+      },
+    })
+    
+    // Completely disable SWC
     config.resolve.alias = {
       ...config.resolve.alias,
+      '@next/swc-linux-x64-gnu': false,
+      '@next/swc-linux-x64-musl': false,
     }
+    
     return config
+  },
+  env: {
+    // Force disable SWC at environment level
+    NEXT_DISABLE_SWC: 'true',
   },
 }
 
